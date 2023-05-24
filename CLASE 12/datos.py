a79570a270f9e6ec8d7632e3f326305e08b76528
paulabelman1 <134552935+paulabelman1@users.noreply.github.com>
2023-05-24 16:44:32 -0400

    datos solicitados

---
+++ CLASE 12/datos.py
@@ -0,0 +1,9 @@
+#ejemplo de uso de gip
+print("ingreso de datos")
+print("----------------/n")
+vNom=input("ingrese su nombre: ")
+vEdad=input("ingrese su edad: ")
+print("-----------------")
+print(f"Su nombre es: {vNom}")
+print(f"Su edad es: {vEdad}")
+print("Programa finalizado..")
